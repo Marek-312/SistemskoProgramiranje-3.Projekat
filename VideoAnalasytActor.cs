007e9cd3b3548ce0add6680caeaab2f8dc57f58a
Marko Djordjevic <maarko.dj@elfak.rs>
2026-06-30 14:41:32 +0200

    ee

--- VideoAnalasytActor.cs
+++ VideoAnalasytActor.cs
@@ -3,24 +3,25 @@ namespace treciProjekat;
 
 public class VideoAnalystActor : ReceiveActor
 {
-    public readonly string _videoID;
+    //public readonly string _videoID;
     //private readonly ML _ml;
     public int pozitivniBrojac;
     public int negativniBrojac;
     public VideoAnalystActor(string videoID)
     {
         //_ml = new ML();
-        _videoID = videoID;
+
         pozitivniBrojac = 0;
         negativniBrojac = 0;
         Receive<ZapocniAnalizuPoruka>(text =>
         {
-            if (ML.AnalizirajTekst(videoID))
+            if (ML.AnalizirajTekst(text.VideoId))
             {
                 pozitivniBrojac++;
 
             }
             else { negativniBrojac--; }
+            _sviKomentari.Add(text.VideoId);
         });
     }
 
